3858b9ae9bd928500d209d8f52eb920b89d451b7
Andre Ricardo <andrericardoweb@gmail.com>
2020-06-22 18:42:04 -0300

    Rename assets

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+let sceneryImage;
+
+function preload() {
+  sceneryImage = loadImage('images/')
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
