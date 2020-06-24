6b7db4e708549daa836f0b3d6637f2646f05ce79
Andre Ricardo <andrericardoweb@gmail.com>
2020-06-23 23:12:11 -0300

    Create class enemy.js

---
+++ scripts/game/enemy.js
@@ -0,0 +1,5 @@
+class Enemy extends Animation {
+  constructor(matrix, image, x, personageWidth, personageHeight, spriteWidth, spriteHeight) {
+    super(matrix, image, x, personageWidth, personageHeight, spriteWidth, spriteHeight)
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,13 +1,47 @@
 
 let sceneryImage;
 let personageImage;
+let enemyImage;
 let scenario;
 let soundtrack;
 let personage;
+let enemy;
+
+const enemyMatrix = [
+  [0, 0],
+  [104, 0],
+  [208, 0],
+  [312, 0],
+  [0, 104],
+  [104, 104],
+  [208, 104],
+  [312, 104],
+  [0, 208],
+  [104, 208],
+  [208, 208],
+  [312, 208],
+  [0, 312],
+  [104, 312],
+  [208, 312],
+  [312, 312],
+  [0, 418],
+  [104, 418],
+  [208, 418],
+  [312, 418],
+  [0, 522],
+  [104, 522],
+  [208, 522],
+  [312, 522],
+  [0, 626],
+  [104, 626],
+  [208, 626],
+  [312, 626],
+]
 
 function preload() {
   sceneryImage = loadImage('images/scenario/forest.png');
   personageImage = loadImage('images/personage/running-witch.png');
+  enemyImage = loadImage('images/enemy/drop.png');
   personage = new Personage(personageImage);
   soundtrack = loadSound('sounds/soundtrack.mp3');
 }
@@ -15,12 +49,14 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   scenario = new Scenario(sceneryImage, 3);
-  //soundtrack.loop();
+  enemy = new Enemy(enemyMatrix, enemyImage, width - 50, 52, 52, 104, 104);
   frameRate(40);
+  //soundtrack.loop();
 }
 
 function draw() {
   scenario.display();
   scenario.move();
   personage.display();
+  enemy.display();
 }
