ee37c0dbeafa759a5e1fadf0db9742f20809e5ce
Andre Ricardo <andrericardoweb@gmail.com>
2020-06-29 09:13:11 -0300

    refactoring

---
+++ scripts/load.js
@@ -0,0 +1,13 @@
+function preload() {
+  sceneryImage = loadImage('images/scenario/forest.png');
+  gameOverImage = loadImage('images/assets/game-over.png');
+
+  personageImage = loadImage('images/personage/running-witch.png');
+
+  enemyImage = loadImage('images/enemy/drop.png');
+  bigEnemyImage = loadImage('images/enemy/troll.png');
+  flyingEnemyImage = loadImage('images/enemy/flying-drop.png');
+
+  soundtrack = loadSound('sounds/soundtrack.mp3');
+  jumpSound = loadSound('sounds/jump.mp3');
+}
